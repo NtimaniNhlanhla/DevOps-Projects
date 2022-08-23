1c532760f0bdd10834c67cba0fe9f7bc51ffd290
Nhlanhla <nhlanhla@vitd.co>
2022-08-23 11:31:44 +0200

    First Commit

---
+++ nodeapp/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send("service is up and running")
+})
+
+app.listen(8080, () =>{
+    console.log("Server is up")
+})
